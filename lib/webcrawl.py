660fd08233708ccd1e5daa00d7f4671960b495de
pohxiangah <pohxiang.yeoh@hiredly.com>
2025-06-12 15:44:07 +0800

    Updated codebase
    Changed from wait untill idle to timeout wait until it loads

--- lib/webcrawl.py
+++ lib/webcrawl.py
@@ -42,7 +42,7 @@ def main():
 
         # 3) Navigate and wait until network is idle
         print(f"Checking CMS requests from: {PAGE_URL}")
-        page.goto(PAGE_URL, wait_until="networkidle")
+        page.goto(PAGE_URL,timeout=60000 ,wait_until="load")
 
         # 4) Give extra time for any late console messages
         page.wait_for_timeout(2000)
